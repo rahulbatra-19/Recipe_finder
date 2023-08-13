107979d7a65792fbd6ff80f98b5a38c24dbc14a8
rahulbatra-19 <batrarahul409@example.com>
2023-08-13 16:10:15 +0530

    Added styles to card component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,15 +1,16 @@
+import styles from '../styles/card.module.css';
 const Card = (props) => {
   return(
-  <div>
+  <div className={styles.card}>
     <div>
-      <img src={props.img} alt={props.name} />
+      <img className={styles.img} src={props.img} alt={props.name} />
     </div>
     <div>
-      <a href="props.url">
-        <h1>{props.name}</h1>
+      <a href={props.url}> 
+        <h3>{props.name}</h3>
       </a>
       <a href={props.url} target="_blank">
-        <button>See More...</button>
+          <button className={styles.btn}>See More...</button>
       </a>
     </div>
     </div>);
